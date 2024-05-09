3813a604f762cea8bfdab8a2f1561f17a9de1c44
ArtKruzhkov <112978976+ArtKruzhkov@users.noreply.github.com>
2024-05-09 19:41:13 +0300

    beautySalon1
    firstCommit(header,carousel)

---
+++ js/common.js
@@ -0,0 +1,69 @@
+$(function () {
+	$('#my-menu').mmenu({
+		extensions: ['widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black'],
+		navbar: {
+			title: '<img src="img/logo-1.svg" alt="logoS&Mitler">'
+		},
+		offCanvas: {
+			position: 'right'
+		}
+	});
+
+	let api = $('#my-menu').data('mmenu');
+	api.bind('opened', function () {
+		$('.hamburger').addClass('is-active');
+	}).bind('closed', function () {
+		$('.hamburger').removeClass('is-active');
+	});
+
+
+
+	$('.carousel-services').on('initialized.owl.carousel', function () {
+		setTimeout(function () {
+			carouselService()
+		}, 100);
+	});
+	$('.carousel-services').owlCarousel({
+		// loop: true,
+		nav: true,
+		smartSpeed: 700,
+		navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+		dots: false,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			800: {
+				items: 2
+			},
+			1200: {
+				items: 3
+			}
+		}
+	});
+
+	function carouselService() {
+		$('.carousel-services-item').each(function () {
+			var ths = $(this),
+				thsh = ths.find('.carousel-services-content').outerHeight();
+			ths.find('.carousel-services-image').css('min-height', thsh);
+		});
+	}
+
+	$('.carousel-services-composition .h3').each(function () {
+		var ths = $(this);
+		ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+	})
+
+	//Resize window
+	$(document).ready(function () {
+		function setEqualHeights() {
+			$('.carousel-services-content').equalHeights();
+		}
+
+		setEqualHeights();
+
+		$(window).resize(setEqualHeights, carouselService);
+	});
+});
